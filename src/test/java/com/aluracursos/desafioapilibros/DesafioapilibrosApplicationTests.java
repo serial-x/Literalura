f0d1f43e4284a567577fd851bbcf9309776fd7c7
Juan E. Perez <adntrelew@gmail.com>
2025-01-16 02:28:35 -0300

    Primer commit

---
+++ src/test/java/com/aluracursos/desafioapilibros/DesafioapilibrosApplicationTests.java
@@ -0,0 +1,14 @@
+package com.aluracursos.desafioapilibros;
+
+import org.junit.jupiter.api.Test;
+import org
+		.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioapilibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
